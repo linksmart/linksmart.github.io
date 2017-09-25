9a8317dcd0b1a02bb33ebf9d53db9d4e7f665bfb
Can Güney Aksakalli <can.gueney.aksakalli@fit.fraunhofer.de>
2017-09-25 13:50:30 +0200

    brick carosels

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ $(function() {
     slidesNavigation: true,
     anchors: anchors,
     menu: '#menu',
+    slideSelector:'fp-slide',
     onLeave: function(index, nextIndex, direction){
       sectionHandler(nextIndex);
     },
@@ -40,3 +41,23 @@ function sectionHandler(index) {
   }
   console.log(index);
 };
+
+var brickCarousel = $('#brickCarousel').carousel({
+  interval: false,
+  keyboard: false
+})
+
+function selectBrick(element, i){
+  $('.brick-wrapper').removeClass('active');
+  $(element).parent().addClass('active');
+  brickCarousel.carousel(i);
+
+  // var scaleTransform = ' scale(1.2)';
+  // $('.brick').each(function( index ) {
+  //   var transform = $(this).css('transform').replace(scaleTransform, '');
+  //   console.log(transform);
+  //   $(this).css('transform', transform);
+  // });
+  // $(element).css('transform', $(element).css('transform') + scaleTransform);
+}
+
