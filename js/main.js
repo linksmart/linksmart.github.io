bb06796dcffb1f9cb4e5efa0b9ce511741bcfb80
Can Güney Aksakalli <can.gueney.aksakalli@fit.fraunhofer.de>
2017-11-22 15:54:53 +0100

    fixing selectCountries method and cleaning the rest

--- js/main.js
+++ js/main.js
@@ -1,69 +1,42 @@
-$(function() {
+$(function () {
   $('#fullpage').fullpage({
     verticalCentered: false,
     navigation: true,
     slidesNavigation: true,
     anchors: anchors,
     menu: '#menu',
-    slideSelector:'fp-slide',
-    responsiveWidth:768,
+    slideSelector: 'fp-slide',
+    responsiveWidth: 768,
     // scrollOverflow:true,
-    onLeave: function(index, nextIndex, direction){
+    onLeave: function (index, nextIndex, direction) {
       sectionHandler(nextIndex);
     },
-    afterLoad: function(anchorLink, index){
+    afterLoad: function (anchorLink, index) {
       sectionHandler(index);
     }
   });
 
-
-  $('#world').load('img/world.svg', function(a) {
-    $(this).children()[0].setAttribute('viewBox',"0 0 1000 1000");
+  $('#world').load('img/world.svg', function (a) {
+    $(this).children()[0].setAttribute('viewBox', "0 0 1000 1000");
+    selectCountries(firstSelectedCountries);
   });
 });
 
 function sectionHandler(index) {
   if (index > 1) {
     $('.navbar')
-      .removeClass('highlighted')
-      // .css('background-color', '#555555')
-      // .css('top', 0);
-
-
+      .removeClass('highlighted');
   }
   else {
     $('.navbar')
-       .addClass('highlighted');
-      // .css('background-color', '')
-      // .css('top', '60px');
-
+      .addClass('highlighted');
   }
   // console.log(index);
-};
-
-var brickCarousel = $('#brickCarousel').carousel({
-  interval: false,
-  keyboard: false
-})
-
-function selectBrick(element, i){
-  $('.brick-wrapper').removeClass('active');
-  $(element).parent().addClass('active');
-  brickCarousel.carousel(i);
-
-  // var scaleTransform = ' scale(1.2)';
-  // $('.brick').each(function( index ) {
-  //   var transform = $(this).css('transform').replace(scaleTransform, '');
-  //   console.log(transform);
-  //   $(this).css('transform', transform);
-  // });
-  // $(element).css('transform', $(element).css('transform') + scaleTransform);
 }
 
-function selectBox(codes){
+function selectCountries(codes) {
   $('#world path').css({ fill: "" });
-  codes.split(',').forEach(function(e) {
+  codes.split(',').forEach(function (e) {
     document.getElementById(e).style.fill = '#fca730';
   });
-
 }
